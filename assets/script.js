f42df4b75cd2cfbc21c70e948fe285dbf6a323a0
Janet Webster <twixmixy@gmail.com>
2023-10-04 15:02:21 -0400

    Card and Modal added, debugging

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,14 @@
-var todaysDate = dayjs().format('MM/DD/YYYY HH:mm:ss');
-console.log (todaysDate)
-$('#today').text(todaysDate) 
+// Function to update the seconds every second
+setInterval(function() {
+    var todaysDate = dayjs().format('MM/DD/YYYY HH:mm:ss');
+    console.log (todaysDate)
+    $('#today').text(todaysDate) 
+}, 1000)
+
+// Modal Dialog
+var myModal = document.getElementById('modal')
+var myInput = document.getElementById('input')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
